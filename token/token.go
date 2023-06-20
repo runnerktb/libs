c3398091a468a909e04b6e3c11d29dd56edec495
lana <iikmaulana369@gmail.com>
2023-06-20 11:59:05 +0700

    Add validation if public_key not found

--- token/token.go
+++ token/token.go
@@ -15,52 +15,90 @@ func ClaimToken(tokens []string) (response AuthorizationInfo, serr serror.SError
 	publicKeyFile := "public_key.pem"
 	publicKeyBytes, err := ioutil.ReadFile(publicKeyFile)
 	if err != nil {
-		return response, serror.New("Public key tidak ditemukan")
-	}
+		secretKey := []byte("um_super_apps")
 
-	publicKey, err := jwt.ParseRSAPublicKeyFromPEM(publicKeyBytes)
-	if err != nil {
-		return response, serror.NewFromError(err)
-	}
+		if tokens == nil {
+			return response, serror.New("Token tidak ditemukan")
+		}
 
-	if tokens == nil {
-		return response, serror.New("Token tidak ditemukan")
-	}
+		tokenString, err := parseToken(tokens[0])
+		if err != nil {
+			return response, serror.NewFromError(err)
+		}
+		decode, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if jwt.GetSigningMethod("HS256") != token.Method {
+				return nil, serror.NewFromError(fmt.Errorf("Unexpected signing method: %v", token.Header["alg"]))
+			}
+			return []byte(secretKey), nil
+		})
+		resToken := decode.Claims.(jwt.MapClaims)
+		if err != nil {
+			return response, serror.NewFromError(err)
+		}
 
-	tokenString, err := parseToken(tokens[0])
-	if err != nil {
-		return response, serror.NewFromError(err)
-	}
-	decode, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return publicKey, nil
-	})
+		tmpUserAccess := []string{}
+		for _, v := range resToken["user_access"].([]interface{}) {
+			tmpUserAccess = append(tmpUserAccess, v.(string))
+		}
+		isOrgAdmin := int(helper.StringToInt(helper.IntToString(int(resToken["isorgadmin"].(float64))), 0))
+		isActive := int(helper.StringToInt(helper.IntToString(int(resToken["isactive"].(float64))), 0))
+		response = AuthorizationInfo{
+			UserID:         fmt.Sprintf("%v", resToken["user_id"]),
+			Username:       fmt.Sprintf("%v", resToken["username"]),
+			IsOrgAdmin:     &isOrgAdmin,
+			IsActive:       isActive,
+			OrganizationId: fmt.Sprintf("%v", resToken["organization_id"]),
+			App:            fmt.Sprintf("%v", resToken["app"]),
+			Email:          fmt.Sprintf("%v", resToken["email"]),
+			Exp:            int(helper.StringToInt(helper.IntToString(int(resToken["exp"].(float64))), 0)),
+			Name:           fmt.Sprintf("%v", resToken["name"]),
+			RealmID:        fmt.Sprintf("%v", resToken["realm_id"]),
+			UserAccess:     tmpUserAccess,
+		}
+		return response, nil
+	} else {
+		publicKey, err := jwt.ParseRSAPublicKeyFromPEM(publicKeyBytes)
+		if err != nil {
+			return response, serror.NewFromError(err)
+		}
 
-	if err != nil {
-		return response, serror.NewFromError(err)
-	}
+		if tokens == nil {
+			return response, serror.New("Token tidak ditemukan")
+		}
 
-	resToken := decode.Claims.(jwt.MapClaims)
-	tmpUserAccess := []string{}
-	for _, v := range resToken["user_access"].([]interface{}) {
-		tmpUserAccess = append(tmpUserAccess, v.(string))
-	}
-	isOrgAdmin := int(helper.StringToInt(helper.IntToString(int(resToken["isorgadmin"].(float64))), 0))
-	isActive := int(helper.StringToInt(helper.IntToString(int(resToken["isactive"].(float64))), 0))
-	response = AuthorizationInfo{
-		UserID:         fmt.Sprintf("%v", resToken["user_id"]),
-		Username:       fmt.Sprintf("%v", resToken["username"]),
-		IsOrgAdmin:     &isOrgAdmin,
-		IsActive:       isActive,
-		OrganizationId: fmt.Sprintf("%v", resToken["organization_id"]),
-		App:            fmt.Sprintf("%v", resToken["app"]),
-		Email:          fmt.Sprintf("%v", resToken["email"]),
-		Exp:            int(helper.StringToInt(helper.IntToString(int(resToken["exp"].(float64))), 0)),
-		Name:           fmt.Sprintf("%v", resToken["name"]),
-		RealmID:        fmt.Sprintf("%v", resToken["realm_id"]),
-		UserAccess:     tmpUserAccess,
-	}
+		tokenString, err := parseToken(tokens[0])
+		if err != nil {
+			return response, serror.NewFromError(err)
+		}
+		decode, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return publicKey, nil
+		})
+		resToken := decode.Claims.(jwt.MapClaims)
+		if err != nil {
+			return response, serror.NewFromError(err)
+		}
 
-	return response, nil
+		tmpUserAccess := []string{}
+		for _, v := range resToken["user_access"].([]interface{}) {
+			tmpUserAccess = append(tmpUserAccess, v.(string))
+		}
+		isOrgAdmin := int(helper.StringToInt(helper.IntToString(int(resToken["isorgadmin"].(float64))), 0))
+		isActive := int(helper.StringToInt(helper.IntToString(int(resToken["isactive"].(float64))), 0))
+		response = AuthorizationInfo{
+			UserID:         fmt.Sprintf("%v", resToken["user_id"]),
+			Username:       fmt.Sprintf("%v", resToken["username"]),
+			IsOrgAdmin:     &isOrgAdmin,
+			IsActive:       isActive,
+			OrganizationId: fmt.Sprintf("%v", resToken["organization_id"]),
+			App:            fmt.Sprintf("%v", resToken["app"]),
+			Email:          fmt.Sprintf("%v", resToken["email"]),
+			Exp:            int(helper.StringToInt(helper.IntToString(int(resToken["exp"].(float64))), 0)),
+			Name:           fmt.Sprintf("%v", resToken["name"]),
+			RealmID:        fmt.Sprintf("%v", resToken["realm_id"]),
+			UserAccess:     tmpUserAccess,
+		}
+		return response, nil
+	}
 }
 
 func parseToken(source string) (token string, err error) {
